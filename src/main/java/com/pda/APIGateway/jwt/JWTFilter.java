9450a6588038e66918c3059e50f1b9413bca9fa4
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-03 16:20:16 +0900

    fix: JWTFilter - WebFlux 적용으로 인해 OncePerRequestFilter to WebFilter로 변경

--- src/main/java/com/pda/APIGateway/jwt/JWTFilter.java
+++ src/main/java/com/pda/APIGateway/jwt/JWTFilter.java
@@ -1,60 +1,46 @@
 package com.pda.APIGateway.jwt;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 
-import java.io.IOException;
 import java.util.List;
 
-    public class JWTFilter extends OncePerRequestFilter {
+public class JWTFilter implements WebFilter {
 
     private final JWTUtil jwtUtil;
 
-    public JWTFilter(JWTUtil jwtUtil){
+    public JWTFilter(JWTUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        String authorization = request.getHeader("Authorization");
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String authorization = exchange.getRequest().getHeaders().getFirst("Authorization");
 
-        if(authorization == null || !authorization.startsWith("Bearer")){
-            System.out.println("token null");
-            filterChain.doFilter(request, response);
-
-            return;
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            return chain.filter(exchange);
         }
-        System.out.println("authorization now");
-        //Bearer 부분 제거 후 순수 토큰 획득
-        String token = authorization.split(" ")[1];
-        //토큰 소멸 시간 검증
-        if(jwtUtil.isExpired(token)){
-            System.out.println(jwtUtil.getId(token));
-            System.out.println(jwtUtil.getRole(token));
-            System.out.println("token expired");
-            filterChain.doFilter(request,response);
 
-            return;
+        String token = authorization.substring(7);
+
+        if (jwtUtil.isExpired(token)) {
+            return chain.filter(exchange);
         }
 
         Long id = jwtUtil.getId(token);
         String role = jwtUtil.getRole(token);
-        System.out.println(role);
 
         JWTToken jwtToken = new JWTToken(id, role);
         Authentication authToken = new UsernamePasswordAuthenticationToken(jwtToken, null, List.of(new SimpleGrantedAuthority(role)));
-        System.out.println(authToken);
-        SecurityContextHolder.getContext().setAuthentication(authToken);
 
-        filterChain.doFilter(request,response);
-
+        exchange.getResponse().getHeaders().set("travelerId", String.valueOf(id));
+        return chain.filter(exchange)
+                .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authToken));
     }
 }
